4d83884c91c7bc0c612f16592be69b1c91860191
Aidan Duong <aidan.duong@gmx.de>
2023-12-14 19:53:45 +0100

    - add chat template as html format - use flan-t5-xxl

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -35,8 +37,9 @@ def get_vectorstore(text_chunks):
     return vectorstore
 
 def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
-    memory = ConversationBufferMemory(memory='chat_history', return_messages=True)
+    #llm = ChatOpenAI()
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=vectorstore.as_retriever(),
@@ -45,20 +48,38 @@ def get_conversation_chain(vectorstore):
     return conversation_chain
 
 
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
+
 def main():
     load_dotenv()
 
-    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+    st.set_page_config(page_title="Chat with your personal PDF AI assistant", page_icon=":books:")
+
+    st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
 
-    st.header("Chat with multiple PDFs :books:")
-    st.text_input("Ask a question about your documents:")
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header(":clipboard: Chat with my personal AI assistant :chart_with_upwards_trend:")
+    user_question = st.text_input("Ask a question about the uploaded documents:")
+    if user_question:
+        handle_userinput(user_question)
 
     with st.sidebar:
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        st.subheader("Your documents :dizzy:")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process' :rocket:", accept_multiple_files=True)
         if st.button("Process"):
             with st.spinner("Processing"):
                 # get pdf text
